64907a5805a63d84db99a43c19c70f676198c5f2
Tobit Misoi <misoitobit@zetech.ac.ke>
2021-05-05 10:08:16 +0300

    add cookie parser

--- src/views/authView/index.tsx
+++ src/views/authView/index.tsx
@@ -1,33 +1,94 @@
-import { createMuiTheme } from '@material-ui/core';
-import { ThemeProvider } from '@material-ui/styles';
+import { createMuiTheme } from '@material-ui/core'
+import { ThemeProvider } from '@material-ui/styles'
+import axios from 'axios'
 import React, { FC } from 'react'
-import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom'
-import Login from '../../components/features/Login/Login';
-import Signup from '../../components/features/signup';
-import Welcome from '../../components/features/welcome';
+import { useDispatch } from 'react-redux'
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  Switch
+} from 'react-router-dom'
+import Login from '../../components/features/Login/Login'
+import Signup from '../../components/features/signup'
+import Welcome from '../../components/features/welcome'
+import Cookie from '../../components/shared/cookie/cookie'
 
+type Props = {}
 
-type Props = {};
-
-const darkTheme = createMuiTheme({
-    palette: {
-        type: 'dark'
-    }
-});
-
-const AuthView: FC<Props> = props => {
-    return (
-        <ThemeProvider theme={darkTheme}>
-            <Router>
-                <Switch>
-                    <Route path="/login" exact component={Login} />
-                    <Route path="/signup" component={Signup} />
-                    <Route path="/" exact component={Welcome} />
-                    <Redirect to="/" />
-                </Switch>
-            </Router>
-        </ThemeProvider>
-    )
+type UserData = {
+  id: string
+  token: string
 }
 
-export default AuthView;
+const darkTheme = createMuiTheme({
+  palette: {
+    type: 'dark'
+  }
+})
+
+const AuthView: FC<Props> = (props) => {
+  const dispatch = useDispatch()
+  const [cookie, setCookie] = React.useState(true)
+
+  // TODO: #37 wrap the definition of 'verifyRequest' in its own useCallback() Hook.
+  // Async request
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const verifyRequest = async (id: string, token: string) => {
+    let resp
+
+    try {
+      resp = await axios.post(
+        `${process.env.REACT_APP_SERVER_URL}/users/verify`,
+        {
+          id,
+          token
+        }
+      )
+    } catch (err) {
+      console.log('[ERROR][AUTH][VERIFY]', err)
+      return
+    }
+    if (!resp.data.access) {
+      localStorage.removeItem('userData')
+      return
+    }
+
+    dispatch({ type: 'LOGIN', payload: { ...resp.data.user } })
+  }
+
+  React.useEffect(() => {
+    const cookieData = localStorage.getItem('cookieData')
+    if (cookieData) setCookie(false)
+
+    const userData = localStorage.getItem('userData')
+    if (!userData) return
+
+    const parseData: UserData = JSON.parse(userData)
+    verifyRequest(parseData.id, parseData.token)
+  }, [verifyRequest])
+
+  return (
+    <ThemeProvider theme={darkTheme}>
+      {cookie && (
+        <Cookie
+          onAccept={() => {
+            localStorage.setItem('cookieData', 'accepted')
+            setCookie(false)
+          }}
+        />
+      )}
+
+      <Router>
+        <Switch>
+          <Route path='/login' exact component={Login} />
+          <Route path='/signup' component={Signup} />
+          <Route path='/' exact component={Welcome} />
+          <Redirect to='/' />
+        </Switch>
+      </Router>
+    </ThemeProvider>
+  )
+}
+
+export default AuthView
