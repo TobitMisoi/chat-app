99bd548367e5cd0504a95aa9accaffd3c39ee6bc
Tobit Misoi <misoitobit@zetech.ac.ke>
2021-05-05 12:36:36 +0300

    upd

--- src/components/side/members/members.tsx
+++ src/components/side/members/members.tsx
@@ -1,13 +1,36 @@
+import { CircularProgress } from '@material-ui/core'
 import React from 'react'
 import { MemberProps } from './interface'
+import Member from './member'
 
 // local imports
 import styles from './styles.module.scss'
 
-const Members: React.FC<MemberProps> = (props) => {
+type Props = {
+  members: MemberProps[]
+  loading: boolean
+}
+
+const Members: React.FC<Props> = (props) => {
   return (
     <div className={styles.container}>
       <p className={styles.title}>Members</p>
+      {props.loading ? (
+        <div className={styles.loading}>
+          <CircularProgress />
+        </div>
+      ) : (
+        <div className={styles.wrapper}>
+          {props.members.map((member) => (
+            <Member
+              key={member?._id}
+              _id={member?._id}
+              username={member?.username}
+              image={member?.image}
+            />
+          ))}
+        </div>
+      )}
     </div>
   )
 }
