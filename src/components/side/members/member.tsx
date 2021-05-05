6dd88137696926828ad59d236ebb2afe6631ede7
Tobit Misoi <misoitobit@zetech.ac.ke>
2021-05-05 12:35:48 +0300

    member

---
+++ src/components/side/members/member.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { MemberProps } from './interface'
+
+// local imports
+import styles from './styles.module.scss'
+
+const Member: React.FC<MemberProps> = (props) => {
+  return (
+    <div className={styles.container}>
+      <img src='https://unsplash.it/102' alt='User' className={styles.image} />
+      <p className={styles.username}>{props.username}</p>
+    </div>
+  )
+}
+
+export default Member
