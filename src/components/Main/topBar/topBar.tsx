ee06e3b04500546f48ab2ab4715019b78b7f1b98
Tobit Misoi <misoitobit@zetech.ac.ke>
2021-05-05 11:19:09 +0300

    topbar

---
+++ src/components/Main/topBar/topBar.tsx
@@ -0,0 +1,26 @@
+import { IconButton } from '@material-ui/core'
+import React from 'react'
+import MenuIcon from '@material-ui/icons/Menu'
+
+// local imports
+import styles from './styles.module.scss'
+
+type Props = {
+  title?: string
+  menuClick: () => void
+}
+
+const TopBar: React.FC<Props> = (props) => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <IconButton className={styles.iconButton} onClick={props.menuClick}>
+          <MenuIcon className={styles.menu} fontSize='large' />
+        </IconButton>
+        <h2 className={styles.title}>{props.title}</h2>
+      </div>
+    </div>
+  )
+}
+
+export default TopBar
