8a5d7a7aa05afc4dd9e34fe0b138161c981f90e0
Tobit Misoi <misoitobit@zetech.ac.ke>
2021-05-05 09:32:58 +0300

    cookie

---
+++ src/components/shared/cookie/cookie.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+// Local imports
+import styles from './styles.module.scss'
+
+type Props = {}
+
+const Cookie: React.FC<Props> = (props) => {
+  return <div className={styles.container}></div>
+}
+
+export default Cookie
