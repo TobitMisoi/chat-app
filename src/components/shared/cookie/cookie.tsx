5a16e8105839af625dea1dbb6b4e8355b95b710a
Tobit Misoi <misoitobit@zetech.ac.ke>
2021-05-05 10:56:16 +0300

    cookie

--- src/components/shared/cookie/cookie.tsx
+++ src/components/shared/cookie/cookie.tsx
@@ -1,12 +1,47 @@
 import React from 'react'
+import CustomButton from '../customButton'
 
 // Local imports
 import styles from './styles.module.scss'
 
-type Props = {}
+type Props = {
+  onAccept: () => void
+}
 
 const Cookie: React.FC<Props> = (props) => {
-  return <div className={styles.container}></div>
+  const [open, setOpen] = React.useState(false)
+
+  return (
+    <div className={styles.container}>
+      {open && (
+        <div className={styles.box}>
+          <div className={styles.title}>Are you hungry?</div>
+          <div className={styles.action}>
+            <CustomButton
+              isPurple
+              small
+              title='Accept cookies'
+              onClick={props.onAccept}
+            />
+            <a
+              href='https://ec.europa.eu/info/cookies_en'
+              target='_blank'
+              className={styles.info}
+              rel='noreferrer'
+            >
+              More information
+            </a>
+          </div>
+        </div>
+      )}
+      <img
+        src='https://unsplash.it/120'
+        className={styles.cookie}
+        onClick={() => setOpen((prev) => !prev)}
+        alt=''
+      />
+    </div>
+  )
 }
 
 export default Cookie
