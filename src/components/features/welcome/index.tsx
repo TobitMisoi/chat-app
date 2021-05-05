b39693b1bcba796698aa898e911834b09f7a63ae
Tobit Misoi <misoitobit@zetech.ac.ke>
2021-05-05 12:52:38 +0300

    welcome modification

--- src/components/features/welcome/index.tsx
+++ src/components/features/welcome/index.tsx
@@ -1,46 +1,54 @@
-import axios from "axios";
-import { FC } from "react";
-import { useDispatch } from "react-redux";
-import { Link } from "react-router-dom";
-import CustomButton from "../../shared/customButton";
-import styles from './styles.module.scss';
+import axios from 'axios'
+import { FC } from 'react'
+import { useDispatch } from 'react-redux'
+import { Link } from 'react-router-dom'
+import CustomButton from '../../shared/customButton'
+import styles from './styles.module.scss'
 
-type Props = {};
+const Welcome: FC = (props) => {
+  const dispatch = useDispatch()
 
-
-
-const Welcome: FC<Props> = props => {
-    const dispatch = useDispatch()
-
-    // Async request
-    const guestRequest = async () => {
-        let res;
-        try {
-            res = await axios.post(``);
-
-        } catch (err) {
-            console.log('[ERROR][AUTH][GUEST]:' + err);
-            return;
-        }
-        dispatch({ type: 'GUEST', payload: { ...res.data.user } })
+  const guestRequest = async () => {
+    let resp
+    try {
+      resp = await axios.post(`${process.env.REACT_APP_SERVER_URL}/users/guest`)
+    } catch (err) {
+      console.log('[ERROR][AUTH][GUEST]', err)
+      return
     }
+    if (!resp.data.access) return
 
-    return (
-        <>
-            <div>
-                <img className={styles.container} src="https://raw.githubusercontent.com/KillianFrappartDev/GroupChat/master/frontend/src/assets/gc-logo.png" alt="logo" />
-                <Link to="/login">
-                    <CustomButton onClick={() => { }} small={false} title="Login" isPurple={false} />
-                </Link>
-                <Link to="/signup">
-                    <CustomButton onClick={() => { }} isPurple={true} title="Signup" small={false} />
-                </Link>
-                <p className={styles.guest} onClick={guestRequest}>
-                    Continue as guest
-                </p>
-            </div>
-        </>
-    )
+    dispatch({ type: 'GUEST', payload: { ...resp.data.user } })
+  }
+
+  return (
+    <div className={styles.container}>
+      <img
+        src='https:unsplash.it/2190'
+        alt='GrouspChat Logo'
+        className={styles.logo}
+      />
+      <Link to='/login'>
+        <CustomButton
+          isPurple={false}
+          small={false}
+          onClick={() => {}}
+          title='Login'
+        />
+      </Link>
+      <Link to='/signup'>
+        <CustomButton
+          isPurple={true}
+          small={false}
+          onClick={() => {}}
+          title='SignUp'
+        />
+      </Link>
+      <p className={styles.guest} onClick={guestRequest}>
+        Continue as guest
+      </p>
+    </div>
+  )
 }
 
-export default Welcome;
+export default Welcome
