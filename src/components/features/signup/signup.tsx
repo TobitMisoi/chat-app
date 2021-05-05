30994b72202948bf829c0edf75db671705ca4f3a
Tobit Misoi <misoitobit@zetech.ac.ke>
2021-05-05 15:48:55 +0300

    signup

--- src/components/features/signup/signup.tsx
+++ src/components/features/signup/signup.tsx
@@ -1,12 +1,45 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { GoHome } from 'react-icons/go'
-import { Checkbox, FormControlLabel, TextField } from '@material-ui/core'
+import {
+  Checkbox,
+  CircularProgress,
+  FormControlLabel,
+  TextField,
+  Snackbar
+} from '@material-ui/core'
+import MuiAlert from '@material-ui/lab/Alert'
+import { useFormik } from 'formik'
+
 // local imports
 import styles from './styles.module.scss'
 import CustomButton from '../../shared/customButton'
 
+type SnackData = {
+  open: boolean
+  message: string | null
+}
+
 const Signup: React.FC = (props) => {
+  const [isLoading, setIsLoading] = React.useState(false)
+  const [snack, setSnack] = React.useState<SnackData>({
+    open: false,
+    message: null
+  })
+  const register = async () => {
+    setIsLoading(true)
+    setSnack({ open: true, message: 'Working on it: Signup not working now' })
+  }
+
+  const formik = useFormik({
+    initialValues: {
+      username: '',
+      email: '',
+      password: ''
+    },
+    onSubmit: () => register()
+  })
+
   return (
     <div className={styles.container}>
       <Link to='/'>
@@ -40,10 +73,27 @@ const Signup: React.FC = (props) => {
           type='submit'
           isPurple
           title='Sigup'
-          onClick={() => {}}
           small={false}
+          onClick={formik.handleSubmit}
         />
       </form>
+      {isLoading && <CircularProgress />}
+      <Link to='/login'>
+        <p className={styles.guest}>{`Already a memeber? Login`}</p>
+      </Link>
+      <Snackbar
+        open={snack.open}
+        autoHideDuration={5000}
+        onClose={() => setSnack({ open: false, message: null })}
+      >
+        <MuiAlert
+          variant='filled'
+          onClose={() => setSnack({ open: true, message: null })}
+          severity='error'
+        >
+          {snack.message}
+        </MuiAlert>
+      </Snackbar>
     </div>
   )
 }
