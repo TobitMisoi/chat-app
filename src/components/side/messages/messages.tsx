65c79cdbd61fb234148db353766cacc2fa560a61
Tobit Misoi <misoitobit@zetech.ac.ke>
2021-05-05 11:06:49 +0300

    add message component

--- src/components/side/messages/messages.tsx
+++ src/components/side/messages/messages.tsx
@@ -3,10 +3,20 @@ import { CircularProgress } from '@material-ui/core'
 
 // local imports
 import styles from './styles.module.scss'
+import Message from './message'
+
+type PropsMessage = {
+  username: string
+  text: string
+  image: string
+  _id: string
+  date: string
+}
 
 type Props = {
   onClick: () => void
   loading: boolean
+  messages: PropsMessage[]
 }
 
 const Messages: React.FC<Props> = (props) => {
@@ -18,7 +28,18 @@ const Messages: React.FC<Props> = (props) => {
             <CircularProgress />
           </div>
         ) : (
-          <div className={styles.wraper}></div>
+          <div className={styles.wraper}>
+            {props.messages.map((item) => (
+              <Message
+                key={item._id}
+                _id={item._id}
+                text={item.text}
+                image={item.image}
+                date={item.date}
+                username={item.username}
+              />
+            ))}
+          </div>
         )}
       </div>
     </div>
