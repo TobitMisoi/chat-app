8b9603920e21258a28e5bf01e38afe33ebb96963
Tobit Misoi <misoitobit@zetech.ac.ke>
2021-05-05 17:17:43 +0300

    lint

--- src/components/side/bottomBar/bottomBar.tsx
+++ src/components/side/bottomBar/bottomBar.tsx
@@ -1,41 +1,45 @@
-import { IconButton, Tooltip } from "@material-ui/core";
-import { BugReport, ExitToApp } from "@material-ui/icons";
-import { FC } from "react";
+/* eslint-disable react/prop-types */
+import { IconButton, Tooltip } from '@material-ui/core'
+import { BugReport, ExitToApp } from '@material-ui/icons'
+import React, { FC } from 'react'
 
-import styles from './styles.module.scss';
+import styles from './styles.module.scss'
 
 type Props = {
-    profileClick: () => void;
-    bugClick: () => void;
-    exitClick: () => void;
+  profileClick: () => void
+  bugClick: () => void
+  exitClick: () => void
 }
 
-
-
-const BottomBar: FC<Props> = props => {
-    return (
-        <div className={styles.container}>
-            <div className={styles.wrapper}>
-                <div className={styles.userBox}>
-                    <Tooltip title="Edit Profile">
-                        <img src="https://raw.githubusercontent.com/KillianFrappartDev/GroupChat/master/frontend/src/assets/gc-logo-symbol-nobg.png" alt="User" className={styles.image} onClick={props.profileClick} />
-                    </Tooltip>
-                    <p className={styles.username}>Tobit Misoi</p>
-                </div>
-            </div>
-            <div className={styles.buttonContainer}>
-                <Tooltip title="Report a bug" placement="top" >
-                    <IconButton className={styles.exitButton} onClick={props.bugClick}>
-                        <BugReport className={styles.exit} />
-                    </IconButton>
-                </Tooltip>
-                <Tooltip title="Logout" placement="top">
-                    <IconButton className={styles.exitButton} onClick={props.exitClick}>
-                        <ExitToApp className={styles.exit} />
-                    </IconButton>
-                </Tooltip>
-            </div>
+const BottomBar: FC<Props> = (props) => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <div className={styles.userBox}>
+          <Tooltip title='Edit Profile'>
+            <img
+              src='https://raw.githubusercontent.com/KillianFrappartDev/GroupChat/master/frontend/src/assets/gc-logo-symbol-nobg.png'
+              alt='User'
+              className={styles.image}
+              onClick={props.profileClick}
+            />
+          </Tooltip>
+          <p className={styles.username}>Tobit Misoi</p>
         </div>
-    )
+      </div>
+      <div className={styles.buttonContainer}>
+        <Tooltip title='Report a bug' placement='top'>
+          <IconButton className={styles.exitButton} onClick={props.bugClick}>
+            <BugReport className={styles.exit} />
+          </IconButton>
+        </Tooltip>
+        <Tooltip title='Logout' placement='top'>
+          <IconButton className={styles.exitButton} onClick={props.exitClick}>
+            <ExitToApp className={styles.exit} />
+          </IconButton>
+        </Tooltip>
+      </div>
+    </div>
+  )
 }
-export default BottomBar;
+export default BottomBar
--- src/components/side/topBar/topBar.tsx
+++ src/components/side/topBar/topBar.tsx
@@ -1,42 +1,43 @@
-import { IconButton, Tooltip } from "@material-ui/core";
-import { Add, ArrowBackIos } from "@material-ui/icons";
-import { FC } from "react";
+/* eslint-disable react/prop-types */
+import { IconButton, Tooltip } from '@material-ui/core'
+import { Add, ArrowBackIos } from '@material-ui/icons'
+import React from 'react'
 
-
-import styles from './styles.module.scss';
+import styles from './styles.module.scss'
 
 type Props = {
-    inChannel: boolean,
-    arrowClick: () => void,
-    plusClick: () => void
+  inChannel: boolean
+  arrowClick: () => void
+  plusClick: () => void
 }
 
-const TopBar: FC<Props> = props => {
-    return (
-        <div className={styles.container}>
-            {props.inChannel ? (
-                <div className={styles.wrapperInChannel}>
-                    <Tooltip title="Back to channels list" placement="bottom" >
-                        <IconButton className={styles.arrowButton} onClick={props.arrowClick}>
-                            <ArrowBackIos className={styles.arrow}></ArrowBackIos>
-                        </IconButton>
-                    </Tooltip>
-                    <h2 className={styles.title}>All Channels</h2>
-                </div>
-            ) : (
-                <div className={styles.wrapperOutChannel}>
-                    <h2 className={styles.title}>Channels</h2>
-                    <Tooltip title="Create Channel" placement="bottom">
-                        <IconButton className={styles.addButton} onClick={props.plusClick}>
-                            <Add className={styles.add} />
-                        </IconButton>
-                    </Tooltip>
-                </div>
-            )
-            }
-
+const TopBar: React.FC<Props> = (props) => {
+  return (
+    <div className={styles.container}>
+      {props.inChannel ? (
+        <div className={styles.wrapperInChannel}>
+          <Tooltip title='Back to channels list' placement='bottom'>
+            <IconButton
+              className={styles.arrowButton}
+              onClick={props.arrowClick}
+            >
+              <ArrowBackIos className={styles.arrow}></ArrowBackIos>
+            </IconButton>
+          </Tooltip>
+          <h2 className={styles.title}>All Channels</h2>
         </div>
-    );
+      ) : (
+        <div className={styles.wrapperOutChannel}>
+          <h2 className={styles.title}>Channels</h2>
+          <Tooltip title='Create Channel' placement='bottom'>
+            <IconButton className={styles.addButton} onClick={props.plusClick}>
+              <Add className={styles.add} />
+            </IconButton>
+          </Tooltip>
+        </div>
+      )}
+    </div>
+  )
 }
 
-export default TopBar;
+export default TopBar
