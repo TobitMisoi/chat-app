ee42e9b5acb302100b7759c364df771cc42eb4fb
Tobit Misoi <misoitobit@zetech.ac.ke>
2021-05-05 11:11:43 +0300

    folder restructure

---
+++ src/components/Main/messageInput/messageInput.tsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import { IconButton, InputBase } from '@material-ui/core'
+import SendIcon from '@material-ui/icons/Send'
+// local imports
+import styles from './styles.module.scss'
+
+type Props = {
+  sendClick: (message: string, date: string) => void
+  onClick: () => void
+}
+
+const MessageInput: React.FC<Props> = (props) => {
+  const [message, setMessage] = React.useState('')
+
+  const getDateString = () => {
+    const monthNames = [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December'
+    ]
+    let dateobj = new Date()
+    let month = monthNames[dateobj.getMonth()]
+    let day = String(dateobj.getDate()).padStart(2, '0')
+    let output = month + ' ' + day + ','
+
+    return `${output} - ${new Date().getHours()}: ${new Date().getMinutes()}`
+  }
+
+  const sendHandler = () => {
+    props.sendClick(message, getDateString())
+    setMessage('')
+  }
+
+  return (
+    <div className={styles.container} onClick={props.onClick}>
+      <InputBase
+        className={styles.input}
+        placeholder={`Write here ...`}
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+        onKeyDown={(e) => {
+          if (e.key === 'Enter') sendHandler()
+        }}
+      />
+      <IconButton className={styles.iconButton} onClick={sendHandler}>
+        <SendIcon className={styles.send} />
+      </IconButton>
+    </div>
+  )
+}
+
+export default MessageInput
---
+++ src/components/Main/onBoard/onBoard.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+// Local imports
+import styles from './styles.module.scss'
+
+type Props = {
+  onClick: () => void
+}
+
+const OnBoard: React.FC<Props> = (props) => {
+  return (
+    <div className={styles.container} onClick={props.onClick}>
+      <div className={styles.wrapper}>
+        <img src='https://unsplash.it/110' alt='logo' className={styles.logo} />
+        <h1 className={styles.logo}>Hello World!</h1>
+        <p className={styles.description}>
+          I really appreciate that you take time to have a look to my work. This
+          app is an instant messaging project that offer the possibility to
+          create and join channels and start a conversation.
+        </p>
+        <ul className={styles.list}>
+          <li>⭐️ Use the "menu" icon on top (Mobile).</li>
+          <li>⭐️ Click on any channel you want to join.</li>
+          <li>⭐️ Create a channel with the "+" icon.</li>
+          <li>⭐️ Send messages with the text input.</li>
+          <li>⭐️ Browse channels with the search input.</li>
+          <li>⭐️ Report a bug with the "bug" icon.</li>
+          <li>⭐️ Click on your profile to edit.</li>
+          <li>⭐️ Use the "exit" icon to logout.</li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default OnBoard
