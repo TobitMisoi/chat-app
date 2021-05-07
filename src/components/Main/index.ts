8a4f5bf23948bce900a9842679d3bb9b7f7c21f8
Tobit Misoi <misoitobit@zetech.ac.ke>
2021-05-07 10:59:19 +0300

    export

---
+++ src/components/Main/index.ts
@@ -0,0 +1,5 @@
+export { default as MessageInput } from './messageInput'
+export { default as Messages } from './messages'
+export { default as OnBoard } from './onBoard'
+export { default as MainTopBar } from './topBar'
+
