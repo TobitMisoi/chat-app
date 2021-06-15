9ec1599f33c7f1a903f0749efca4ff6717d48f52
Tobit Misoi <misoitobit@zetech.ac.ke>
2021-06-15 14:28:57 +0300

    update

--- src/components/features/signup/signup.tsx
+++ src/components/features/signup/signup.tsx
@@ -1,84 +1,154 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { GoHome } from 'react-icons/go'
+import React from "react";
+import { Link, useHistory } from "react-router-dom";
+import { GoHome } from "react-icons/go";
 import {
   Checkbox,
   CircularProgress,
   FormControlLabel,
   TextField,
-  Snackbar
-} from '@material-ui/core'
-import MuiAlert from '@material-ui/lab/Alert'
-import { useFormik } from 'formik'
+  Snackbar,
+} from "@material-ui/core";
+import * as Yup from "yup";
+import MuiAlert from "@material-ui/lab/Alert";
+import { useFormik } from "formik";
 
 // local imports
-import styles from './styles.module.scss'
-import CustomButton from '../../shared/customButton/customButton'
+import styles from "./styles.module.scss";
+import CustomButton from "../../shared/customButton/customButton";
+import axios from "axios";
+import { useDispatch } from "react-redux";
 
 type SnackData = {
-  open: boolean
-  message: string | null
-}
+  open: boolean;
+  message: string | null;
+};
 
 const Signup: React.FC = () => {
-  const [isLoading, setIsLoading] = React.useState(false)
+  const dispatch = useDispatch();
+  const history = useHistory();
+
+  const [isLoading, setIsLoading] = React.useState(false);
   const [snack, setSnack] = React.useState<SnackData>({
     open: false,
-    message: null
-  })
-  const register = async () => {
-    setIsLoading(true)
-    setSnack({ open: true, message: 'Working on it: Signup not working now' })
-  }
+    message: null,
+  });
+  const [checked, setChecked] = React.useState(false);
+
+  // Async request
+  const signupSubmit = async (
+    checked: boolean,
+    email: string,
+    password: string,
+    username: string
+  ) => {
+    let resp;
+    try {
+      resp = await axios.post(`http://localhost:5000/users`, {
+        checked,
+        email: email.toLowerCase(),
+        password: password.toLowerCase(),
+        username,
+      });
+    } catch (error) {
+      console.log("[ERROR][AUTH][SIGNUP] ", error);
+      setIsLoading(false);
+      return;
+    }
+
+    if (!resp.data) {
+      setSnack({ open: true, message: resp.data.message });
+      setIsLoading(false);
+      return;
+    }
+
+    if (checked) {
+      localStorage.setItem(
+        "userData",
+        JSON.stringify({ id: resp.data.userId, token: resp.data.userToken })
+      );
+    }
+    dispatch({ type: "LOGIN", payload: resp.data.message });
+    history.push("");
+    setIsLoading(false);
+  };
 
   const formik = useFormik({
     initialValues: {
-      username: '',
-      email: '',
-      password: ''
+      username: "",
+      email: "",
+      password: "",
     },
-    onSubmit: () => register()
-  })
+    validationSchema: Yup.object({
+      username: Yup.string()
+        .min(2, "Must be 2 characters at least")
+        .required("Required")
+        .max(12, "Can not exceed 12 characters"),
+      email: Yup.string().email("Invalid email address").required("Required"),
+      password: Yup.string()
+        .min(6, "Must be 6 characters at least")
+        .required("Required")
+        .max(20, "Can not exceed 20 characters"),
+    }),
+    onSubmit: (values) =>
+      signupSubmit(checked, values.email, values.password, values.username),
+  });
 
   return (
     <div className={styles.container}>
-      <Link to='/'>
+      <Link to="/">
         <GoHome />
       </Link>
-      <form action='' className={styles.form}>
+      <form action="" className={styles.form}>
         <TextField
           className={styles.input}
-          id='username'
-          label='Username'
-          variant='outlined'
+          id="username"
+          label="Username"
+          variant="outlined"
+          helperText={formik.touched.username && formik.errors.username}
+          error={formik.touched.username && !!formik.errors.username}
+          {...formik.getFieldProps("username")}
         />
         <TextField
           className={styles.input}
-          id='email'
-          label='Email'
-          variant='outlined'
+          id="email"
+          label="Email"
+          variant="outlined"
+          helperText={formik.touched.email && formik.errors.email}
+          error={formik.touched.email && !!formik.errors.email}
+          {...formik.getFieldProps("email")}
         />
         <TextField
           className={styles.input}
-          id='password'
-          label='Password'
-          variant='outlined'
+          id="password"
+          label="Password"
+          type="password"
+          variant="outlined"
+          helperText={formik.touched.password && formik.errors.password}
+          error={formik.touched.password && !!formik.errors.password}
+          {...formik.getFieldProps("password")}
         />
         <FormControlLabel
           className={styles.check}
-          control={<Checkbox />}
-          label='Remamber me'
+          control={
+            <Checkbox
+              checked={checked}
+              onChange={() => setChecked((prev) => !prev)}
+              name="checkedB"
+              color="primary"
+            />
+          }
+          label="Remamber me"
         />
         <CustomButton
-          type='submit'
+          type="submit"
           isPurple
-          title='Sigup'
+          title="Signup"
           small={false}
           onClick={formik.handleSubmit}
         />
       </form>
       {isLoading && <CircularProgress />}
-      <Link to='/login'>
+      <Link to="/login">
         <p className={styles.guest}>{`Already a memeber? Login`}</p>
       </Link>
       <Snackbar
@@ -87,15 +157,15 @@ const Signup: React.FC = () => {
         onClose={() => setSnack({ open: false, message: null })}
       >
         <MuiAlert
-          variant='filled'
+          variant="filled"
           onClose={() => setSnack({ open: true, message: null })}
-          severity='error'
+          severity="error"
         >
           {snack.message}
         </MuiAlert>
       </Snackbar>
     </div>
-  )
-}
+  );
+};
 
-export default Signup
+export default Signup;
