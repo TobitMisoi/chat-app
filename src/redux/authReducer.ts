525c30cfd8983950ae6018470c0db8a8872b1ba3
Tobit Misoi <misoitobit@zetech.ac.ke>
2021-07-22 16:29:07 +0300

    redux/authReducer: LOGOUT

--- src/redux/authReducer.ts
+++ src/redux/authReducer.ts
@@ -1,32 +1,53 @@
 type UserState = {
-    isLogged: boolean,
-    id: string | null,
-    username: string | null,
-    image: string | null,
-    token: string | null
+  isLogged: boolean;
+  id: string | null;
+  username: string | null;
+  image: string | null;
+  token: string | null;
 };
 
 type UserAction = {
-    type: string,
-    payload: {
-        id: string;
-        username: string;
-        image: string;
-        token: string | null;
-    }
+  type: string;
+  payload: {
+    id: string;
+    username: string;
+    image: string;
+    token: string | null;
+  };
 };
 
-
-const initialState: UserState = { isLogged: false, id: null, username: null, image: null, token: null }
+const initialState: UserState = {
+  isLogged: false,
+  id: null,
+  username: null,
+  image: null,
+  token: null,
+};
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 const reducer = (state = initialState, action: UserAction) => {
-    switch (action.type) {
-        case 'LOGIN':
-            return { ...state, isLogged: true, username: action.payload.username, image: action.payload.image, token: action.payload.token, id: action.payload.id }
-        default:
-            return state;
-    }
-}
+  switch (action.type) {
+    case "LOGIN":
+      return {
+        ...state,
+        isLogged: true,
+        username: action.payload.username,
+        image: action.payload.image,
+        token: action.payload.token,
+        id: action.payload.id,
+      };
+    case "LOGOUT":
+      return {
+        ...state,
+        isLogged: false,
+        username: action.payload.username,
+        image: action.payload.image,
+        token: null,
+        id: action.payload.id,
+      };
+    default:
+      return state;
+  }
+};
 
 export default reducer;
