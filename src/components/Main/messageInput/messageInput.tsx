4691f87ee84417d94913345ca45573ae3bb7ccb2
Tobit Misoi <misoitobit@zetech.ac.ke>
2021-07-22 19:14:34 +0300

    messages: fixes

--- src/components/Main/messageInput/messageInput.tsx
+++ src/components/Main/messageInput/messageInput.tsx
@@ -1,45 +1,45 @@
 /* eslint-disable react/prop-types */
-import React from 'react'
-import { IconButton, InputBase } from '@material-ui/core'
-import SendIcon from '@material-ui/icons/Send'
+import React from "react";
+import { IconButton, InputBase } from "@material-ui/core";
+import SendIcon from "@material-ui/icons/Send";
 // local imports
-import styles from './styles.module.scss'
+import styles from "./styles.module.scss";
 
 type Props = {
-  sendClick: (message: string, date: string) => void
-  onClick: () => void
-}
+  sendClick: (message: string, date: string) => void;
+  onClick: () => void;
+};
 
 const MessageInput: React.FC<Props> = (props) => {
-  const [message, setMessage] = React.useState('')
+  const [message, setMessage] = React.useState("");
 
   const getDateString = () => {
     const monthNames = [
-      'January',
-      'February',
-      'March',
-      'April',
-      'May',
-      'June',
-      'July',
-      'August',
-      'September',
-      'October',
-      'November',
-      'December'
-    ]
-    const dateobj = new Date()
-    const month = monthNames[dateobj.getMonth()]
-    const day = String(dateobj.getDate()).padStart(2, '0')
-    const output = month + ' ' + day + ','
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    const dateObj = new Date();
+    const month = monthNames[dateObj.getMonth()];
+    const day = String(dateObj.getDate()).padStart(2, "0");
+    const output = month + " " + day + ",";
 
-    return `${output} - ${new Date().getHours()}: ${new Date().getMinutes()}`
-  }
+    return `${output} - ${new Date().getHours()}: ${new Date().getMinutes()}`;
+  };
 
   const sendHandler = () => {
-    props.sendClick(message, getDateString())
-    setMessage('')
-  }
+    props.sendClick(message, getDateString());
+    setMessage("");
+  };
 
   return (
     <div className={styles.container} onClick={props.onClick}>
@@ -49,14 +49,14 @@ const MessageInput: React.FC<Props> = (props) => {
         value={message}
         onChange={(e) => setMessage(e.target.value)}
         onKeyDown={(e) => {
-          if (e.key === 'Enter') sendHandler()
+          if (e.key === "Enter") sendHandler();
         }}
       />
       <IconButton className={styles.iconButton} onClick={sendHandler}>
         <SendIcon className={styles.send} />
       </IconButton>
     </div>
-  )
-}
+  );
+};
 
-export default MessageInput
+export default MessageInput;
