c09e2996e54942a6097d9278365cb837f1948d29
Tobit Misoi <misoitobit@zetech.ac.ke>
2021-07-22 16:09:32 +0300

    Signup: mods

--- src/components/features/signup/signup.tsx
+++ src/components/features/signup/signup.tsx
@@ -1,44 +1,86 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { GoHome } from 'react-icons/go'
+import React from "react";
+import { Link, useHistory } from "react-router-dom";
+import { GoHome } from "react-icons/go";
 import {
   Checkbox,
   CircularProgress,
   FormControlLabel,
   TextField,
-  Snackbar
-} from '@material-ui/core'
-import MuiAlert from '@material-ui/lab/Alert'
-import { useFormik } from 'formik'
+  Snackbar,
+} from "@material-ui/core";
+import MuiAlert from "@material-ui/lab/Alert";
+import { useFormik } from "formik";
 
 // local imports
-import styles from './styles.module.scss'
-import CustomButton from '../../shared/customButton/customButton'
+import styles from "./styles.module.scss";
+import CustomButton from "../../shared/customButton/customButton";
+import axios from "axios";
+import { apiConfig } from "../../../config/api";
+import { useDispatch } from "react-redux";
 
 type SnackData = {
-  open: boolean
-  message: string | null
-}
+  open: boolean;
+  message: string | null;
+};
 
 const Signup: React.FC = () => {
-  const [isLoading, setIsLoading] = React.useState(false)
+  const [isLoading, setIsLoading] = React.useState(false);
   const [snack, setSnack] = React.useState<SnackData>({
     open: false,
-    message: null
-  })
-  const register = async () => {
-    setIsLoading(true)
-    setSnack({ open: true, message: 'Working on it: Signup not working now' })
-  }
+    message: null,
+  });
+  const [checked, setChecked] = React.useState(false);
+
+  const dispatch = useDispatch();
+  const history = useHistory();
+
+  const register = async (
+    checked: boolean,
+    email: string,
+    password: string,
+    username: string
+  ) => {
+    setIsLoading(true);
+
+    let resp;
+    try {
+      resp = await axios.post(`${apiConfig.url}/users/signup`, {
+        checked,
+        email: email.toLowerCase(),
+        password: password.toLowerCase(),
+        username,
+      });
+    } catch (err) {
+      console.log("[ERROR][AUTH][SIGNUP] ", err);
+      setIsLoading(false);
+      return;
+    }
+
+    if (!resp.data.access) {
+      setSnack({ open: true, message: resp.data.message });
+      setIsLoading(false);
+      return;
+    }
+    if (checked) {
+      localStorage.setItem(
+        "userData",
+        JSON.stringify({ id: resp.data.user.id, token: resp.data.user.token })
+      );
+    }
+
+    dispatch({ type: "LOGIN", payload: { ...resp.data.user } });
+    history.push("");
+    setIsLoading(false);
+  };
 
   const formik = useFormik({
     initialValues: {
-      username: '',
-      email: '',
-      password: ''
+      username: "",
+      email: "",
+      password: "",
     },
-    onSubmit: () => register()
-  })
+    onSubmit: (val) => register(checked, val.email, val.password, val.username),
+  });
 
   return (
     <div className={styles.container}>
@@ -51,35 +93,49 @@ const Signup: React.FC = () => {
           id='username'
           label='Username'
           variant='outlined'
+          helperText={formik.touched.username && formik.errors.username}
+          {...formik.getFieldProps("username")}
         />
         <TextField
           className={styles.input}
           id='email'
           label='Email'
           variant='outlined'
+          helperText={formik.touched.email && formik.errors.email}
+          {...formik.getFieldProps("email")}
         />
         <TextField
           className={styles.input}
           id='password'
           label='Password'
           variant='outlined'
+          type='password'
+          helperText={formik.touched.password && formik.errors.password}
+          {...formik.getFieldProps("password")}
         />
         <FormControlLabel
           className={styles.check}
-          control={<Checkbox />}
-          label='Remamber me'
+          control={
+            <Checkbox
+              checked={checked}
+              onChange={() => setChecked((prev) => !prev)}
+              name='checkedB'
+              color='primary'
+            />
+          }
+          label='Remember me'
         />
         <CustomButton
           type='submit'
           isPurple
-          title='Sigup'
+          title='Signup'
           small={false}
           onClick={formik.handleSubmit}
         />
       </form>
       {isLoading && <CircularProgress />}
       <Link to='/login'>
-        <p className={styles.guest}>{`Already a memeber? Login`}</p>
+        <p className={styles.guest}>{`Already a member? Login`}</p>
       </Link>
       <Snackbar
         open={snack.open}
@@ -95,7 +151,7 @@ const Signup: React.FC = () => {
         </MuiAlert>
       </Snackbar>
     </div>
-  )
-}
+  );
+};
 
-export default Signup
+export default Signup;
